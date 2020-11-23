e0902cdace807978583e6a328a80bfc2298d8807
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-23 13:22:16 -0500

    still trying to debug why heroku not working

--- html_routes.js
+++ html_routes.js
@@ -1,17 +1,18 @@
-const p = require("path");
+const path = require("path");
 
 module.exports = (app) => {
 
     // getting the notes html file
 
     app.get("/notes", function (req, res) {
-        res.sendFile(p.join(__dirname, '/public/notes.html'));
+        res.sendFile(path.join(__dirname, '/public/notes.html'));
     });
 
     // getting the index html file
 
     app.get("*", function (req, res) {
-        res.sendFile(p.join(__dirname, '/public/index.html'));
+        res.sendFile(path.join(__dirname, '/public/index.html'));
     });
 
 };
+
